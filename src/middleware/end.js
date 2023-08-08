cf27fee78330ca608d4394e58f135d8c7f68768a
ZhuAiQuan <hadeszaq@outlook.com>
2023-08-08 10:41:47 +0800

    fix

--- src/middleware/end.js
+++ src/middleware/end.js
@@ -1,6 +1,6 @@
 export default async function(ctx, next) {
   await next();
-  if ((!ctx.body.data || !Object.keys(ctx.body.data).length) && ctx.body.code === 200 && ctx.request.url !== '/') {// 最终返回结果判断
+  if ((!ctx.body?.data || !Object.keys(ctx.body?.data).length) && ctx.body?.code === 200 && ctx.request?.url !== '/') {// 最终返回结果判断
     ctx.body = {
       code: 500,
       message: 'system error'
